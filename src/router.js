75137a204c12cc9a8f33be7d43ae97810ac7c701
gaetanovenuto <gaetanovenuto99@live.it>
2024-11-21 13:11:53 +0100

    Modificato contenuto Router.js

--- src/router.js
+++ src/router.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import HomePage from './pages/HomePage.vue'
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
