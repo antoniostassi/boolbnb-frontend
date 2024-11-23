455ef527fe7d0ab099a0db9747faa79ae15f2a61
Alessandro Sclafani <scla.alessandro@gmail.com>
2024-11-23 20:07:26 +0100

    created a function closeCanvas() that clicks on the close button of canvas, this is user experience and the inputs weren't working because of the overlay. Changed the variables to show the form modal and added the closeCanvas function on every link of the offcanvas

--- src/components/HeaderComponents/OffCanvasBody.vue
+++ src/components/HeaderComponents/OffCanvasBody.vue
@@ -1,5 +1,4 @@
 <script>
-
 import axios from 'axios';
 import { store, api } from '../../store'
 export default {
@@ -12,30 +11,34 @@ export default {
   components: {
   },
   methods: {
+    closeCanvas(){
+        document.getElementById("close-canvas").click()
+    }
   },
+  
 }
 </script>
 
 <template>
-  <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+  <div class="offcanvas offcanvas-end w-100" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
             <div class="offcanvas-header">
                 <h5 class="text-center ms-3" id="offcanvasRightLabel">
                     Menù |
                 </h5>
                 <h5 v-if="!this.api.isLoggedIn" class="text-center ms-1" id="offcanvasRightLabel">
-                    <button @click="showLoginForm = true; isRegistration = false" class="btn offcanvas-btn-outline-primary">Accedi</button>
-                    <button @click="showLoginForm = true; isRegistration = true" class="btn offcanvas-btn-primary">Registrati</button>
+                    <button @click="store.showLoginForm = true; store.isRegistration = false ; this.closeCanvas()" class="btn offcanvas-btn-outline-primary">Accedi</button>
+                    <button @click="store.showLoginForm = true; store.isRegistration = true; this.closeCanvas() " class="btn offcanvas-btn-primary">Registrati</button>
                 </h5>
                 <h5 v-else class="text-center ms-1" id="offcanvasRightLabel">
                     Benvenuto {{ api.user.firstname + ' ' + api.user.lastname }}
                 </h5>
-                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                <button type="button" id="close-canvas" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
             <div class="offcanvas-body">
                 <!-- CORPO DELL'OFFCANVAS IN MOBILE DEVICES -->
 
                 <div class="logo-container">
-                    <router-link to="/" class="d-flex align-items-center">
+                    <router-link to="/" class="d-flex align-items-center" @click="this.closeCanvas()">
                         <img src="/img/BoolBnB Logo.png" alt="BoolBnB Logo" class="logo ms-3" />
                     </router-link>
                 </div>
@@ -44,7 +47,7 @@ export default {
                 <div class="navbar d-flex flex-column align-items-start">
                     <ul class="nav flex-column fs-5">
                         <li class="nav-item">
-                            <router-link
+                            <router-link @click="this.closeCanvas()"
                                 v-if="this.api.isLoggedIn"
                                 :to="{ name: 'profile-page' }"
                                 class="nav-link">
@@ -52,24 +55,25 @@ export default {
                             </router-link>
                         </li>
                         <li v-if="this.api.isLoggedIn" class="nav-item">
-                            <router-link :to="{ name: 'your-apartments' }" class="nav-link">
+                            <router-link :to="{ name: 'your-apartments' }" class="nav-link" @click="this.closeCanvas()">
                                 Le tue strutture
                             </router-link>
                         </li>
                         <li v-if="this.api.isLoggedIn" class="nav-item">
-                            <a class="nav-link" href="#" @click="userLogout">
+                            <a class="nav-link" href="#" @click="userLogout; this.closeCanvas()" >
                                 Logout
                             </a>
                         </li>
                         <hr>
                         <li v-for="(link, index) in store.navbarLinks" :key="index" class="nav-item">
-                            <router-link :to="link.href" class="nav-link">{{ link.name }}</router-link>
+                            <router-link @click="this.closeCanvas()" :to="link.href" class="nav-link">{{ link.name }}</router-link>
                         </li>
                     </ul>
                     
                 </div>
             </div>
         </div>
+
 </template>
 
 <style lang="scss">
