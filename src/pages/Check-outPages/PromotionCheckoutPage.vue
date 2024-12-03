1f6b36d52aea3e57906a365f538424fe6ad61d51
Alessandro Sclafani <scla.alessandro@gmail.com>
2024-12-03 12:10:51 +0100

    ultimated checkout page with validation

--- src/pages/Check-outPages/PromotionCheckoutPage.vue
+++ src/pages/Check-outPages/PromotionCheckoutPage.vue
@@ -7,11 +7,14 @@ export default {
             api,
             store,
             selectedPromotionId: null, // ID della promozione selezionata
-            today: null
+            today: null,
+            cardNumber: null,
+            cardCVV: null,
+            CardError: false,
         };
     },
     mounted(){
-        // this.api.redirectIfNotCreated();
+        this.api.redirectIfNotCreated();
         this.takeTodayDate();
     },
     methods:{
@@ -35,25 +38,32 @@ export default {
             this.today = `${year}-${month}-${day}`;
         },
         Payment(){
-            axios.post('https://payments.sandbox.braintree-api.com/graphql',
-            {
-                query: `query { ping }` 
-            },
-            {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer sandbox_9qq3yksf_58rry6f7bkwwx3gq', 
-                'Braintree-Version': '2023-01-01' 
-            },
-            withCredentials: false
-            })
-            .then(response => {
-                this.api.createApartment();
-                console.log('Appartamento creato con successo')
-            })
-            .catch(error => {
-                console.log(error);
-            });
+            this.CardError = false;
+            this.store.formSubmitted = true
+            if(this.cardCVV.toString().length == 3 && this.cardNumber.toString().length == 16){
+                axios.post('https://payments.sandbox.braintree-api.com/graphql',
+                {
+                    query: `query { ping }` 
+                },
+                {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer sandbox_9qq3yksf_58rry6f7bkwwx3gq', 
+                    'Braintree-Version': '2023-01-01' 
+                },
+                withCredentials: false
+                })
+                .then(response => {
+                    this.api.createApartment();
+                    
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+            }
+            else{
+                this.CardError = true;
+            }
         }
     }
 }
@@ -64,38 +74,47 @@ export default {
     <div class="row py-4" >
         <div class="col-12">
             <h3 class="fw-bold">
-                Hai scelto
+                Hai scelto: 
                 <span v-if="api.selectedPromotionId == 3">Bronze</span>
                 <span v-else-if="api.selectedPromotionId == 2">Silver</span>
                 <span v-else="api.selectedPromotionId == 1">Gold</span>
             </h3>
             <div class="payment">
-                <form @submit.prevent="Payment()">
-                    <div class="py-2">
+                <form @submit.prevent="Payment()" class="border rounded p-4">
+                    <div class="py-2 pe-2">
                         <label for="name">Nome:</label>
                         <input id="name" class="form-control" type="text" disabled :value="api.user.firstname">
                     </div>
-                    <div class="py-2">
+                    <div class="py-2 pe-2">
                         <label for="surname">Cognome:</label>
                         <input id="surname" class="form-control" type="text" disabled :value="api.user.lastname">
                     </div>
-                    <div class="py-2">
+                    <div class="py-2 pe-2">
                         <label for="email">E-mail:</label>
                         <input id="email" class="form-control" type="email" disabled :value="api.user.email">
                     </div>
-                    <div class="py-2">
-                        <label for="card">Numero carta:</label>
-                        <input id="card" class="form-control" type="number" minlength="16" maxlength="16" placeholder="Inserisci il numero della tua carta..." required>
+                    <div class="d-flex w-100 flex-wrap justify-content-between align-items-center">
+                        <div class="col-12 col-md-6 pe-2">
+                            <label for="card">Numero carta:</label>
+                            <input id="card" class="form-control" type="number" v-model="cardNumber" placeholder="Inserisci il numero della tua carta..." required>
+                        </div>
+                        <div class="col-6 col-md-3 pe-2">
+                            <label for="expiring_date">Data di scadenza:</label>
+                            <input id="expiring_date" :min="today" :value="today" required class="form-control" type="date">
+                        </div>
+                        <div class="col-6 col-md-3 pe-2">
+                            <label for="name">CVV:</label>
+                            <input class="form-control" type="number" v-model="cardCVV" required>
+                        </div>
+                        <div v-show="store.APIError" class="text-danger fw-bold">Errore: si è verificato un errore inaspettato, verrai reindirizzato alla tua dashboard</div>
+                        <div v-show="CardError" class="text-danger fw-bold">Errore: controlla i dati della carta</div>
+                        <div class="col-12 pe-2 text-end py-2 fs-5 fw-bold">
+                            Totale da pagare: <span class="price p-1 rounded">{{ api.promotions[api?.selectedPromotionId - 1]?.price }}€</span>
+                        </div>
                     </div>
-                    <div class="py-2">
-                        <label for="expiring_date">Data di scadenza:</label>
-                        <input id="expiring_date" :min="today" :value="today" required class="form-control" type="date">
+                    <div class="w-100 text-end">
+                        <button class="btn text-white" :disabled="store.formSubmitted">Procedi al pagamento</button>
                     </div>
-                    <div class="py-2">
-                        <label for="name">CVV:</label>
-                        <input class="form-control" type="number" min="3" maxlength="3" required>
-                    </div>
-                    <button>Procedi al pagamento</button>
                 </form>
             </div>
         </div>
@@ -105,6 +124,14 @@ export default {
 <style lang="scss" scoped>
 @import '@/assets/scss/partials/variables.scss';
 @import '@/assets/scss/partials/mixins.scss';
-
+button{
+    background-color: $color-violet;
+    &:hover{
+        background-color: $color-lightviolet;
+    }
+}
+.price{
+    background-color:rgb(132, 216, 132)
+}
 </style>
 
